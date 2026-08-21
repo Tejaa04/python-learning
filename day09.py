8d8ac596c4b0b69fff56e50d034ea8d7bd7ddadd
teja <151869273+Tejaa04@users.noreply.github.com>
2026-08-21 09:46:36 +0530

    Conditional Statements

--- day09.py
+++ day09.py
@@ -1,6 +1,27 @@
-n = int(input('Enter the day number: '))
+# 1. If-Elif Condition (HackerRank Problem)
+n1 = int(input().strip())
 
-match n:
+if n1 % 2 != 0:
+    print("Weird")
+elif 2 <= n1 <= 5:
+    print("Not Weird")
+elif 6 <= n1 <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
+
+
+# 2. Leap Year Check
+def is_leap(year):
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+year = int(input())
+print(is_leap(year))
+
+# 3. Match-Case Day Mapper
+n3 = int(input('Enter the day number: '))
+
+match n3:
     case 1:
         print('Sunday')
     case 2:
